3a747647d2a685bff07bb338a7fd426ea63ea8f1
javohir <javohir@example.com>
2023-05-30 13:05:39 +0300

    bir

---
+++ java.js
@@ -0,0 +1,37 @@
+
+
+
+do{
+
+
+let soat = +prompt ('soat yozing')
+
+if(soat <= 13){
+    alert('abetgi soat 1')
+}else if(soat <= 14){
+    alert('abetgi soat 2')
+}else if(soat <= 15){
+    alert('abetgi soat 3')
+}else if(soat <= 16){
+    alert('abetgi soat 4')
+}else if(soat <= 17){
+    alert('abetgi soat 5')
+}else if(soat <= 18){
+    alert('abetgi soat 6')
+}else if(soat <= 19){
+    alert('abetgi soat 7')
+}else if(soat <= 20){
+    alert('abetgi soat 8')
+}else if(soat <= 21){
+    alert('abetgi soat 9')
+}else if(soat <= 22){
+    alert('abetgi soat 10')
+}else if(soat <= 23){
+    alert('abetgi soat 11')
+}else if(soat <= 24){
+    alert('abetgi soat 12')
+}
+
+}while(soat <= 24){
+    alert(' ')
+}
